fec957b7b9253974b915917e73578cd3645f0496
RAMGOD99 <R@m@nn!!1>
2019-09-21 12:59:04 -0400

    Homework

---
+++ Hello_World.py
@@ -0,0 +1,36 @@
+'''import math
+#print(int(15))
+#print(int("3f", 16))
+#print(int(15.56))
+#print(float("-11.24e8"))
+#print(round(15.56,1))
+#print(bool())
+#print(str('x'))
+#print(chr(64))
+#x = 4
+#print(repr(x))
+#print(bytes([72 , 9 , 64]))
+#print(list("abc"))
+#print(dict([(3, "three"),(1 , "one")]))
+#print(set(["one", "two"]))
+x = 1.2+8 + math.sin(5)
+print(x)
+
+a = b = c = 0
+y,z, r =9.2, -7.6, 0
+a,b = b,a
+'''
+
+import datetime
+
+currentDT = datetime.datetime.now ()
+print ( str ( currentDT ) )
+hh = currentDT.hour
+a = 'Buenos'
+b = 'Buenas'
+if hh < 12:
+  print(a + ' ' + 'Dias')
+if hh > 12 and hh < 16:
+    print(b + ' ' + 'Tardes')
+else:
+    print(b + ' ' + 'Noches')
